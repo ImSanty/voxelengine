b3356426f6ac47acf9f452328031499e6f0f0b59
ImSanty <santiaguero936@gmail.com>
2025-10-19 21:42:51 -0300

    Add ambient occlusion support to Vertex class; initialize ao value in constructors

--- src/main/java/Cube/Vertex.java
+++ src/main/java/Cube/Vertex.java
@@ -6,10 +6,16 @@ import org.lwjgl.util.vector.Vector3f;
 public class Vertex {
   public Vector3f positions, normals;
   public Vector2f uvs;
+  public float ao; // 0 (dark) .. 1 (full light)
 
   public Vertex(Vector3f positions, Vector2f uvs, Vector3f normals) {
+    this(positions, uvs, normals, 1f);
+  }
+
+  public Vertex(Vector3f positions, Vector2f uvs, Vector3f normals, float ao) {
     this.positions = positions;
     this.uvs = uvs;
     this.normals = normals;
+    this.ao = ao;
   }
 }
